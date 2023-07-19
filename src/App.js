de540d094fa70c7662c0519528a770b65baccdc7
mbellorin2 <mbellorin2@gmail.com>
2023-07-18 21:28:36 -0300

    Revert "updat-index-app"
    This reverts commit 5f5945c7fcea7ec7dd00966f552f3d3b3d89fd1a.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
-import Wrap from "./Wrap";
-import Footer from "./Footer";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-      <Wrap />
-      <Footer />
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
