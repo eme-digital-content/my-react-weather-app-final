97db7d45e5ad8c6e3a5fe455b76139a7d2b04ac4
mbellorin2 <mbellorin2@gmail.com>
2023-07-24 20:09:51 -0300

    delete-extra-buttons
    out of scope

--- src/Cities.js
+++ src/Cities.js
@@ -3,30 +3,10 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
-export default function Cities() {
+export default function Search() {
   return (
-    <div className="Cities">
-      <button id="current-location" className="current-location">
-        Current location
-      </button>
-      <div className="popular">
-        <h6 className="popular-text">Popular</h6>
-        <button className="popular-city" id="new-york">
-          New York
-        </button>
-        <button className="popular-city" id="paris">
-          Paris
-        </button>
-        <button className="popular-city" id="toronto">
-          Toronto
-        </button>
-        <button className="popular-city" id="tokyo">
-          Tokyo
-        </button>
-        <button className="popular-city" id="lima">
-          Lima
-        </button>
-      </div>
+    <div className="Search">
+      <br></br>
     </div>
   );
 }
