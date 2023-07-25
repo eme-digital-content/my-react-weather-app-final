959b1196f8f652ba75b4b5e05c8f8f616cc47c1d
mbellorin2 <mbellorin2@gmail.com>
2023-07-25 14:40:57 -0300

    Update Cities.js

--- src/Cities.js
+++ src/Cities.js
@@ -3,10 +3,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
-export default function Search() {
-  return (
-    <div className="Search">
-      <br></br>
-    </div>
-  );
+export default function Cities() {
+  return <div className="Search"></div>;
 }
