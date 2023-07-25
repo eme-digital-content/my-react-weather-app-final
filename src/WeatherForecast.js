a0da1027dfc6f4594e6caced5691e979cb614578
mbellorin2 <mbellorin2@gmail.com>
2023-07-25 14:22:58 -0300

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./WeatherForecast.css";
+import "./weatherforecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
