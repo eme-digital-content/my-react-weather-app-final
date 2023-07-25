511f229c86acc52107cc98f6a4fad23d1e68a47d
mbellorin2 <mbellorin2@gmail.com>
2023-07-25 14:18:56 -0300

    Revert "edit-default-city"
    This reverts commit d5f600ea12d8782e66ec96e8579ece79ee9e3acc.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Header />
-        <Weather defaultCity="Porto Alegre" />
+        <Weather defaultCity="New York" />
       </div>
     </div>
   );
