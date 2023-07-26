e04d43a8983d0f23afb0a1733dc4a1933bb77f2b
Mariana B.A <112900716+mbellorin2@users.noreply.github.com>
2023-07-25 21:23:41 -0300

    apikey-update

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "cb286bad3607984b41ed10c8de5cf00e";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
